d6b39169ed3b080b07342443b7a8856f5a606d2d
Ayush Chaudhary <147637466+ayush23chaudhary@users.noreply.github.com>
2025-08-21 12:07:55 +0530

    Create 2042-MaximumProductDifferenceBetweenTwoPairs

---
+++ 2042-MaximumProductDifferenceBetweenTwoPairs/2042-MaximumProductDifferenceBetweenTwoPairs.py
@@ -0,0 +1,12 @@
+# Last updated: 8/21/2025, 12:07:55 PM
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        ma=[]; mi=[]
+        for i in range(2):
+            ma.append(max(nums))
+            mi.append(min(nums))
+            nums.remove(max(nums))
+            nums.remove(min(nums))
+        a=ma[0]*ma[1]
+        b=mi[0]*mi[1]
+        return a-b
