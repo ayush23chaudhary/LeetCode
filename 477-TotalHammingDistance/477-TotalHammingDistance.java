b9ac479940116951dedf566f4a87a7a2c05d3510
Ayush Chaudhary <147637466+ayush23chaudhary@users.noreply.github.com>
2025-08-21 12:09:29 +0530

    Create 477-TotalHammingDistance

---
+++ 477-TotalHammingDistance/477-TotalHammingDistance.java
@@ -0,0 +1,14 @@
+// Last updated: 8/21/2025, 12:09:28 PM
+class Solution {
+    public int totalHammingDistance(int[] nums) {
+         int result = 0;
+        for(int i = 0; i<32; i++){
+              int count_ones= 0;
+            for(int j = 0; j<nums.length; j++){
+                count_ones+=(nums[j]>>i)&1; //count no. of 1's at a specific position
+            }
+            result +=(count_ones*(nums.length-count_ones));
+        }
+        return result;  
+    }
+}
