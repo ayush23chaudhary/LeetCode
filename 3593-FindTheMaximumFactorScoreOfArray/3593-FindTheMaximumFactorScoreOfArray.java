f477288b98255c96237409e270c1db325e1d146d
Ayush Chaudhary <147637466+ayush23chaudhary@users.noreply.github.com>
2025-08-21 12:06:07 +0530

    Create 3593-FindTheMaximumFactorScoreOfArray

---
+++ 3593-FindTheMaximumFactorScoreOfArray/3593-FindTheMaximumFactorScoreOfArray.java
@@ -0,0 +1,39 @@
+// Last updated: 8/21/2025, 12:06:06 PM
+class Solution {
+    public long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public long lcm(long a, long b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    public long maxScore(int[] arr) {
+        long arrayGCD = 0;
+        long arrayLCM = 1;
+
+        for (int num : arr) {
+            arrayGCD = gcd(arrayGCD, num);
+            arrayLCM = lcm(arrayLCM, num);
+        }
+
+        long maxFactor = arrayGCD * arrayLCM;
+
+        for (int i = 0; i < arr.length; i++) {
+            long subGCD = 0;
+            long subLCM = 1;
+
+            for (int j = 0; j < arr.length; j++) {
+                if (i != j) {
+                    subGCD = gcd(subGCD, arr[j]);
+                    subLCM = lcm(subLCM, arr[j]);
+                }
+            }
+
+            maxFactor = Math.max(maxFactor, subGCD * subLCM);
+        }
+
+        return maxFactor;
+    }
+}
