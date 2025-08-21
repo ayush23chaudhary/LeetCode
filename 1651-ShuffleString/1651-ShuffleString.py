22f18968975a9e02462bf60304abbe22684669d4
Ayush Chaudhary <147637466+ayush23chaudhary@users.noreply.github.com>
2025-08-21 12:08:29 +0530

    Create 1651-ShuffleString

---
+++ 1651-ShuffleString/1651-ShuffleString.py
@@ -0,0 +1,8 @@
+# Last updated: 8/21/2025, 12:08:29 PM
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        l=[]
+        c=0
+        for i in range(len(indices)):
+           l.append(s[indices.index(i)])
+        return "".join(l)
