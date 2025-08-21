1a6594f589dcb2f80cb5bc14d0ee50049c77bf1c
Ayush Chaudhary <147637466+ayush23chaudhary@users.noreply.github.com>
2025-08-21 12:08:27 +0530

    Create 1693-SumOfAllOddLengthSubarrays

---
+++ 1693-SumOfAllOddLengthSubarrays/1693-SumOfAllOddLengthSubarrays.py
@@ -0,0 +1,11 @@
+# Last updated: 8/21/2025, 12:08:26 PM
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        # corner case
+        
+        res = 0; freq = 0; n = len(arr)
+        for i in range(n):
+            freq = freq-(i+1)//2+(n-i+1)//2
+            res += freq*arr[i]
+        return res
+        
