3f25120ada02883a2ba3315bdac1ef73a77e69f4
Ayush Chaudhary <147637466+ayush23chaudhary@users.noreply.github.com>
2025-08-21 12:07:52 +0530

    Create 2058-ConcatenationOfArray

---
+++ 2058-ConcatenationOfArray/2058-ConcatenationOfArray.py
@@ -0,0 +1,9 @@
+# Last updated: 8/21/2025, 12:07:52 PM
+class Solution(object):
+    def getConcatenation(self, n):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return n+n
+        
