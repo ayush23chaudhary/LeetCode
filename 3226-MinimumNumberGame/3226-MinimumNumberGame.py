c17b533252bdb7faecbc934fce573d2e08c750fe
Ayush Chaudhary <147637466+ayush23chaudhary@users.noreply.github.com>
2025-08-21 12:06:28 +0530

    Create 3226-MinimumNumberGame

---
+++ 3226-MinimumNumberGame/3226-MinimumNumberGame.py
@@ -0,0 +1,9 @@
+# Last updated: 8/21/2025, 12:06:28 PM
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        arr=[]
+        nums.sort()
+        for i in range(0,len(nums),2):
+            arr.append(nums[i+1])
+            arr.append(nums[i])
+        return arr
