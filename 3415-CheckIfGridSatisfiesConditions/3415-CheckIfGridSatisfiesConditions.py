1d158f9048c7db3ca01fa02bad3ff80a4346ba7d
Ayush Chaudhary <147637466+ayush23chaudhary@users.noreply.github.com>
2025-08-21 12:06:17 +0530

    Create 3415-CheckIfGridSatisfiesConditions

---
+++ 3415-CheckIfGridSatisfiesConditions/3415-CheckIfGridSatisfiesConditions.py
@@ -0,0 +1,11 @@
+# Last updated: 8/21/2025, 12:06:16 PM
+class Solution:
+    def satisfiesConditions(self, grid: List[List[int]]) -> bool:
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if i+1!= len(grid) and grid[i][j]!=grid[i+1][j]:
+                    return False
+                elif j+1!= len(grid[i]) and grid[i][j] == grid[i][j + 1]:
+                    return False
+        return True
+                
