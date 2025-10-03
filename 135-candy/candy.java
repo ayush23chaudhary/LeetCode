4b2ce1df85cf1a1ee83b8f74e8e61aacab668501
Ayush Chaudhary <147637466+ayush23chaudhary@users.noreply.github.com>
2025-10-03 12:07:35 +0530

    Time: 3 ms (80.26%) | Memory: 45.8 MB (71.12%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,24 +1,34 @@
 class Solution {
     public int candy(int[] ratings) {
-        int ans=0;
-        int[] candies= new int[ratings.length];
-
-        for(int i=0;i<ratings.length;i++){
-            candies[i]=1;
-        }
-        for(int i=1;i<ratings.length;i++){
-            if(ratings[i]>ratings[i-1]){
-                candies[i]=candies[i-1]+1;
+        int candy=1;
+        int i=1;
+        int n=ratings.length;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                candy++;
+                i++;
+                continue; 
+            }
+            //slope bdhega / ghtega
+            int up=1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                up++;
+                candy+=up;
+                i++;
             }
 
-        }
-        for(int i=ratings.length-1;i>0;i--){
-            if(ratings[i-1]>ratings[i]){
-                candies[i-1]= Math.max(candies[i]+1,candies[i-1]);
-
+            //-
+            int down=1;
+            while(i<n && ratings[i]<ratings[i-1]){
+                candy+=down;
+                down++;
+                i++;
             }
-            ans+=candies[i-1];
+
+            if(up<down){
+                candy+=(down-up);
+            }
         }
-        return (ans+candies[ratings.length-1]);
+        return candy;
     }
 }
