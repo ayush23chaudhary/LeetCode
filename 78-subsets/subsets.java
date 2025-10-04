64ca4c224941e4ea792fa804976d58306e748fda
Ayush Chaudhary <147637466+ayush23chaudhary@users.noreply.github.com>
2025-10-04 10:57:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (66.89%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans= new ArrayList<>();
+        List<Integer> l1= new ArrayList<>();
+        // int p=(int) Math.pow(2,nums.length);
+        subset(nums,ans,l1,0);
+        return ans;
+
+    }
+    static void subset(int[] nums,List<List<Integer>> ans,List<Integer> l1,int idx){
+        if(nums.length==idx){
+            ans.add(new ArrayList<>(l1));
+            return;
+        }
+        
+        l1.add(nums[idx]);
+        subset(nums,ans,l1,idx+1);
+        l1.remove(l1.size()-1);
+        subset(nums,ans,l1,idx+1);
+    }
+}
