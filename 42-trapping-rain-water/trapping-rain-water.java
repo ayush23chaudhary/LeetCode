e2e82400b6b695587787fe43f9b15e35b3dd2260
Ayush Chaudhary <147637466+ayush23chaudhary@users.noreply.github.com>
2025-12-03 16:49:11 +0530

    Time: 1 ms (58.07%) | Memory: 48.7 MB (6.41%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int trap(int[] height) {
+        return traph(height);
+    }
+    public static int traph(int[] arr){
+        int  n=arr.length;
+        int[] left= new int[n];
+        int[] right= new int[n];
+        // left array making
+        left[0]=arr[0];
+        for (int i=1;i<n;i++){
+            left[i]=Math.max(left[i-1], arr[i]);
+        }
+        //right array makig
+        right[n-1]=arr[arr.length-1];
+        for(int i=arr.length-2;i>=0;i--){
+            right[i]=Math.max(right[i+1],arr[i]);
+        }
+        // calc total area
+        int sum=0;
+        for(int i=0; i<arr.length;i++){
+            sum+=Math.min(left[i],right[i])-arr[i];
+        }
+        return sum;
+
+    }
+}
