e76d933c1dcaf34da1b959d3ab1913af7937225d
Ayush Chaudhary <147637466+ayush23chaudhary@users.noreply.github.com>
2026-01-29 23:35:16 +0530

    Time: 67 ms (11.49%) | Memory: 84.5 MB (31.08%) - LeetSync

---
+++ 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    public int maxFrequency(int[] nums, int target) {
+        int initialCount = 0;
+        for (int num : nums) {
+            if (num == target) {
+                initialCount++;
+            }
+        }
+        
+        int[] processedArray = nums.clone();
+        int maxIncrease = 0;
+
+        for (int value = 1; value <= 50; value++) {
+            if (value == target) continue;
+
+            int difference = target - value;
+            int currentCount = 0;
+            int localMax = 0;
+
+            for (int num : nums) {
+                if (num == value) {
+                    currentCount++;
+                } else if (num == target) {
+                    currentCount--;
+                }
+
+                if (currentCount < 0) {
+                    currentCount = 0;
+                }
+
+                if (currentCount > localMax) {
+                    localMax = currentCount;
+                }
+            }
+
+            if (localMax > maxIncrease) {
+                maxIncrease = localMax;
+            }
+        }
+
+        int finalFrequency = initialCount + maxIncrease;
+        return finalFrequency;
+    }
+}
