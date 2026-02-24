39f648f6688e3ff2245f9fe2e40f642e979d21bd
Ayush Chaudhary <147637466+ayush23chaudhary@users.noreply.github.com>
2026-02-24 11:13:52 +0530

    Time: 732 ms (5.02%) | Memory: 53.2 MB (10.32%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.java
+++ 1558-course-schedule-iv/course-schedule-iv.java
@@ -10,20 +10,46 @@ class Solution {
         List<Boolean> ans= new ArrayList<>();
         int i=0;
         for(int[] a:queries){
-            ans.add(BFS(a[0],a[1],graph));
+            ans.add(DFS(a[0],a[1],graph));
             i++;
         }
         return ans;
 
         
     }
-    public boolean BFS(int src, int des, List<List<Integer>> graph){
-        Queue<Integer> q= new LinkedList<>();
-        HashSet<Integer> visited= new HashSet<>();
-        q.add(src);
-        while (!q.isEmpty()) {
+    // public boolean BFS(int src, int des, List<List<Integer>> graph){
+    //     Queue<Integer> q= new LinkedList<>();
+    //     HashSet<Integer> visited= new HashSet<>();
+    //     q.add(src);
+    //     while (!q.isEmpty()) {
+	// 		// 1. remove
+	// 		int r = q.poll();
+	// 		// 2. Ignore if Already visited
+	// 		if (visited.contains(r)) {
+	// 			continue;
+	// 		}
+	// 		// 3. Makred visited
+	// 		visited.add(r);
+	// 		// 4. self work
+	// 		if (r == des) {
+	// 			return true;
+	// 		}
+	// 		// 5. Add unvisited nbrs
+	// 		for (int nbrs : graph.get(r)) {
+	// 			if (!visited.contains(nbrs)) {
+	// 				q.add(nbrs);
+	// 			}
+	// 		}
+	// 	}
+	// 	return false;
+    // }
+    public boolean DFS(int src, int des,List<List<Integer>> graph) {
+		Stack<Integer> st = new Stack<>();
+		HashSet<Integer> visited = new HashSet<>();
+		st.push(src);
+		while (!st.isEmpty()) {
 			// 1. remove
-			int r = q.poll();
+			int r = st.pop();
 			// 2. Ignore if Already visited
 			if (visited.contains(r)) {
 				continue;
@@ -37,10 +63,11 @@ class Solution {
 			// 5. Add unvisited nbrs
 			for (int nbrs : graph.get(r)) {
 				if (!visited.contains(nbrs)) {
-					q.add(nbrs);
+					st.push(nbrs);
 				}
 			}
 		}
 		return false;
-    }
+
+	}
 }
