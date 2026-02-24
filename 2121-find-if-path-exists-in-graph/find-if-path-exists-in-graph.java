eeec00c1ae799e78a8d9e52163accc75294658a1
Ayush Chaudhary <147637466+ayush23chaudhary@users.noreply.github.com>
2026-02-24 10:53:11 +0530

    Time: 112 ms (58.84%) | Memory: 265.9 MB (79.43%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,38 +1,45 @@
 class Solution {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
-        // Create adjacency list for the graph
-        List<List<Integer>> graph = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            graph.add(new ArrayList<>());
+    public boolean validPath(int n, int[][] edges, int src, int des) {
+        List<List<Integer>> graph= new ArrayList<>();
+        for(int i=0;i<n;i++){
+            graph.add(i,new ArrayList<>());
         }
-
-        // Populate the graph with edges
-        for(int[] arr : edges){
-            graph.get(arr[0]).add(arr[1]);
-            graph.get(arr[1]).add(arr[0]);
+        for(int i=0;i<edges.length;i++){
+            int a= edges[i][0];
+            int b= edges[i][1];
+            graph.get(a).add(b);
+            graph.get(b).add(a);
         }
+        Set<Integer> vis= new HashSet<>();
+        //Ararys.fill(vis,-1);
+        return BFS(src,des,vis,graph);
 
-        // Array to keep track of visited nodes
-        boolean[] visited = new boolean[n];
-
-        return dfs(source, destination, visited, graph);
     }
+    public boolean BFS(int src, int des,Set<Integer> vis,List<List<Integer>> graph) {
+		Queue<Integer> q = new LinkedList<>();
+		HashSet<Integer> visited = new HashSet<>();
+		q.add(src);
+		while (!q.isEmpty()) {
+			// 1. remove
+			int r = q.poll();
+			// 2. Ignore if Already visited
+			if (visited.contains(r)) {
+				continue;
+			}
+			// 3. Makred visited
+			visited.add(r);
+			// 4. self work
+			if (r == des) {
+				return true;
+			}
+			// 5. Add unvisited nbrs
+			for (int nbrs : graph.get(r)) {
+				if (!visited.contains(nbrs)) {
+					q.add(nbrs);
+				}
+			}
+		}
+		return false;
 
-    public boolean dfs(int node, int des, boolean[] visited, List<List<Integer>>graph){
-        if(node == des){
-            return true;
-        }
-
-        visited[node] = true;
-
-        // Explore neighbors
-        for (int neighbor : graph.get(node)) {
-            if (!visited[neighbor]) {
-                if (dfs(neighbor, des, visited, graph)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+	}
 }
