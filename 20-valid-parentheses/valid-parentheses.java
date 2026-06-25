f56fd129618536deb809aa114106739847b256ab
Ayush Chaudhary <147637466+ayush23chaudhary@users.noreply.github.com>
2026-06-25 10:48:20 +0530

    Time: 3 ms (87.42%) | Memory: 43.1 MB (84.97%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,25 +1,33 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '[' || ch == '{') {
-                stack.push(ch);
-            } else {
-                if (stack.isEmpty()) {
+        Stack<Character> st= new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch= s.charAt(i);
+            if(ch=='{' || ch=='[' || ch=='('){
+                st.push(ch);
+
+            }
+            else {
+                
+                if(st.isEmpty()){
                     return false;
                 }
-                char top = stack.pop();
-                if (ch == ')' && top != '(') {
+                char top= st.pop();
+                if( ch=='}' && top!='{'){
                     return false;
                 }
-                if (ch == ']' && top != '[') {
+                else if( ch==']' && top!='['){
                     return false;
                 }
-                if (ch == '}' && top != '{') {
+                else if( ch==')' && top!='('){
                     return false;
                 }
+                //else return true;
+
             }
         }
-        return stack.isEmpty();
+        if(st.isEmpty()) return true;
+        return false;
+
     }
 }
